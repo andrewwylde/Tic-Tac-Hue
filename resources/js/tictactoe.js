98285c9576f1255e6649ec352c71078f52bbc8c9
astoellis <astoellis@gmail.com>
2015-05-05 21:51:07 -0400

    All done for tonight, added some js to start the logic flowing

--- resources/js/tictactoe.js
+++ resources/js/tictactoe.js
@@ -1,5 +1,31 @@
-$(document).ready(function() {
+
+var Game = function(){
+  this.gameBoard = {
+  'top-left': null,
+  'top-middle': null,
+  'top-right': null,
+  'center-left': null,
+  'center': null,
+  'center-right': null,
+  'bottom-left': null,
+  'bottom-middle': null,
+  'bottom-right': null
+  };
+  k
+
+}
 
 
 
-});
+//testing
+
+gameOne = new Game();
+console.log(gameOne);
+
+
+
+
+// $(document).ready(function() {
+
+
+// });
