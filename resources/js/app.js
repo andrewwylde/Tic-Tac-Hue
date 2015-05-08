a8f4438b8fbcf8fb5c0eed060933cadbca5223a4
astoellis <astoellis@gmail.com>
2015-05-08 17:29:26 -0400

    installed bootstrap w/ bower

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,43 +1,34 @@
 var TicTacJoe = TicTacJoe || {};
 
-
 $(document).ready(function() {
 
 turnNumber = 8;
 playerNum = 1;
 
-
 domEl = $('#gameboard');
 game = TicTacJoe.Game;
 game.init(domEl);
 
-
-$('.start').click(function () {
+$('#start').click(function () {
   game = TicTacJoe.Game;
   game.init(domEl);
+  $(this).toggleClass('hidden');
 });
-
 
 $('.square').click(function() {
 
 if(turnNumber < 9){
 
-
   var xcoord = $(this).data('x');
   var ycoord = $(this).data('y');
-
   var square = game.gameBoard[xcoord][ycoord];
 
   turnNumber ++;
-
-
 
 } else {
 alert('Game Over');
 $('button:hidden').toggleClass('hidden');
 }
-
-
 
 /*This is the area where I test for win conditions*/
 
