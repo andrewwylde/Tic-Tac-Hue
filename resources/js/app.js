a8f928d6e006e860f36686c093f4864ea6852f75
astoellis <astoellis@gmail.com>
2015-05-12 13:08:32 -0400

    Fixed indentation everywhere

--- resources/js/app.js
+++ resources/js/app.js
@@ -27,54 +27,54 @@ $(document).ready(function() {
 
 
 
-$('#gameboard').on('click', '.square',function() {
+  $('#gameboard').on('click', '.square',function() {
 
 
 
-  if(turnNumber < 5){
-    xcoord = $(this).data('x');
-    ycoord = $(this).data('y');
+    if(turnNumber < 5){
+      xcoord = $(this).data('x');
+      ycoord = $(this).data('y');
 
-    $(this).addClass(playerNum);
+      $(this).addClass(playerNum);
 
-    game.setBoard(ycoord,xcoord,playerNum);
-
-
-
-  } else if (turnNumber < 9) {
-    xcoord = $(this).data('x');
-    ycoord = $(this).data('y');
-
-    game.setBoard(ycoord,xcoord,playerNum);
-    game.testWinner(playerNum);
-
-  } else {
-
-    xcoord, ycoord = $(this).data('x'), $(this).data('y');
-
-    $(this).addClass(playerNum);
-
-    game.setBoard(ycoord,xcoord,playerNum);
-
-    alert('Game Over');
-    $('button:hidden').toggleClass('hidden');
-    $('#spacer').hide();
-  }
+      game.setBoard(ycoord,xcoord,playerNum);
 
 
 
-  $('.alert-success').html('You placed a square at a place: [' + xcoord  + ',' + ycoord +']' + '<br>[' + game.gameBoard[0] + ']' + '<br>[' + game.gameBoard[1] + ']' + '<br>[' + game.gameBoard[2] + ']');
-  $('.alert-success').alert();
+    } else if (turnNumber < 9) {
+      xcoord = $(this).data('x');
+      ycoord = $(this).data('y');
 
-  turnNumber++;
+      game.setBoard(ycoord,xcoord,playerNum);
+      game.testWinner(playerNum);
 
-  if (playerNum === 1) {
-    $(this).addClass('x');
-    $(this).html('<img src="resources/images/playX.png" alt="" class="marker" > ');
-  } else {
-    $(this).addClass('o');
-    $(this).html('<img src="resources/images/playO.png" alt="" class="marker">');
-  }
+    } else {
+
+      xcoord, ycoord = $(this).data('x'), $(this).data('y');
+
+      $(this).addClass(playerNum);
+
+      game.setBoard(ycoord,xcoord,playerNum);
+
+      alert('Game Over');
+      $('button:hidden').toggleClass('hidden');
+      $('#spacer').hide();
+    }
+
+
+
+    $('.alert-success').html('You placed a square at a place: [' + xcoord  + ',' + ycoord +']' + '<br>[' + game.gameBoard[0] + ']' + '<br>[' + game.gameBoard[1] + ']' + '<br>[' + game.gameBoard[2] + ']');
+    $('.alert-success').alert();
+
+    turnNumber++;
+
+    if (playerNum === 1) {
+      $(this).addClass('x');
+      $(this).html('<img src="resources/images/playX.png" alt="" class="marker" > ');
+    } else {
+      $(this).addClass('o');
+      $(this).html('<img src="resources/images/playO.png" alt="" class="marker">');
+    }
 
 
   //Test if turn# is even, if not, switch
